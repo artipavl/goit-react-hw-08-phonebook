cfd108896190b226714ec74863ff0b5f164eee35
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-09 01:04:02 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,51 @@
 import { Section } from "components/Section/Section";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authLogin } from "redux/auth/operations/authLogin";
 
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+
+    const submitForm = async e => {
+      e.preventDefault();
+      const submitButton = e.currentTarget.elements.submitButton;
+      submitButton.disabled = true;
+
+      try {
+        await dispatch(authLogin({ email, password }));
+        submitButton.disabled = false;
+      } catch (error) {
+        console.log(error);
+        submitButton.disabled = false;
+      }
+
+      setEmail('');
+      setPassword('');
+  };
+  
   return (
     <Section title="Register">
-      <form>
-        <input type="email" name="email" />
+      <form onSubmit={e => submitForm(e)}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
         <br />
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
         <br />
-        <button type="submit">submit</button>
+        <button type="submit" name="submitButton">
+          submit
+        </button>
       </form>
     </Section>
   );
