4ad92ae1e4efeea07b286ff3736ea10ade51be12
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-09 01:44:44 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,8 +2,17 @@ import { Section } from 'components/Section/Section';
 import { Phonebook } from 'components/Phonebook/Phonebook';
 import { ContactList } from 'components/Contacts/ContactList';
 import { Filter } from 'components/Filter/Filter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAll } from 'redux/contacts/operations/fetchAll';
 
 export const Contacts = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAll());
+  }, [dispatch]);
+
   return (
     <div>
       <Section title="Phonebook">
