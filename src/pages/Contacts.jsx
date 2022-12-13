990502564246220dee9d12a581f7d87628332905
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-13 23:10:00 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,16 +2,17 @@ import { Section } from 'components/Section/Section';
 import { Phonebook } from 'components/Phonebook/Phonebook';
 import { ContactList } from 'components/Contacts/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchAll } from 'redux/contacts/operations/fetchAll';
 
 export const Contacts = () => {
   const dispatch = useDispatch();
-
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  
   useEffect(() => {
-    dispatch(fetchAll());
-  }, [dispatch]);
+    isLoggedIn && dispatch(fetchAll());
+  }, [dispatch, isLoggedIn]);
 
   return (
     <div>
