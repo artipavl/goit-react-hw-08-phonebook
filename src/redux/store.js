ec5920a93bfd6ee816af0096e791747da5d667a8
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-13 23:10:22 +0200

    add persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/authSlice';
-// import persistStore from 'redux-persist/es/persistStore';
+import persistStore from 'redux-persist/es/persistStore';
 import { contactsReducer } from './contacts/contactsSlice';
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { filterReducer } from './filterSlice';
 
 export const store = configureStore({
@@ -18,12 +18,12 @@ export const store = configureStore({
     auth: authReducer,
     filter: filterReducer,
   },
-  // middleware: getDefaultMiddleware =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
