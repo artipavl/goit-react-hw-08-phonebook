a4eadab17b03889a23181486b667a48e464d6f83
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-15 22:51:14 +0000

    Deploying to gh-pages from @ artipavl/goit-react-hw-08-phonebook@cefc977f307765b86474f875598e8166d234f70e 🚀

---
+++ static/js/415.6766bec3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,t,c){c.r(t);var r=c(4190),o=c(1294),a=c(184);t.default=function(){return(0,a.jsx)(o.$,{children:(0,a.jsx)(r.X6,{as:"h1",textAlign:"center",children:'Hello, this project was carried out as part of the "React" practice.'})})}}}]);
+//# sourceMappingURL=415.6766bec3.chunk.js.map
