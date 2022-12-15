87dc52920190fc7238d0b5a3e80ec77e788e13e7
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-15 23:27:45 +0200

    Update authCurrent.js

--- src/redux/auth/operations/authCurrent.js
+++ src/redux/auth/operations/authCurrent.js
@@ -4,7 +4,6 @@ import { getCurrent } from 'API/API';
 export const authCurrent = createAsyncThunk('auth/current', async (_, thunkAPI) => {
   try {
     const response = await getCurrent();
-    // setAuthHeader(response.data.token);
     return response.data;
   } catch (error) {
     thunkAPI.rejectWithValue(error.message);
